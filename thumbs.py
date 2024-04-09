a88b48235b9299a4fb79a4e15bb120c7002be947
Prajyot Dange <prajyotdange32@gmail.com>
2024-04-09 16:06:31 +0530

    thumbs updated

--- thumbs.py
+++ thumbs.py
@@ -1,23 +1,82 @@
-import mediapipe
 import cv2
 from time import sleep
 from cvzone.HandTrackingModule import HandDetector
+from pynput.keyboard import Key , Controller
+from pynput import keyboard
 
 camera = cv2.VideoCapture(0)
-detector = HandDetector(detectionCon=0.8 , )
+detector = HandDetector(detectionCon=0.8 ,)
+myKeyboard = Controller()
 
-while True:
-    success , img = camera.read()
+pressed = {}
+
+def on_press(key): 
+    if key not in pressed: # Key was never pressed before
+        pressed[key] = False
     
-    hand , img = detector.findHands(img)
-    
-    if hand :
-        print(hand)
-    
-    cv2.imshow("a" , cv2.flip(img , 1))  
-    
-    sleep(2)
+    if not pressed[key]: 
+        pressed[key] = True
+        # print('Key %s pressed' % key) 
+        
+def on_release(key):  # Same logic
+    if pressed[key]:
+        pressed[key] = False
+        # print('Key %s released' %key)
+        
+
+with keyboard.Listener( on_press=on_press, on_release=on_release) as listener:
+    while camera.isOpened():
+        success , img = camera.read()
+        hand , img = detector.findHands(img)
+        
+        if len(hand) == 2 :
+            
+            fingers = detector.fingersUp(hand[1])
+            
+            # nitroo
+            if ((fingers[3] == 0 and fingers[4] == 0) and (fingers[1] == 1 and fingers[2] == 1)): 
+                print("nitro")
+                myKeyboard.press(Key.alt)
+            
+            # down key    
+            elif all(v == 0 for v in fingers):
+                print("down")
+                myKeyboard.release(Key.left)
+                myKeyboard.release(Key.alt)
+                myKeyboard.release(Key.right)
+                myKeyboard.release(Key.up)
+                myKeyboard.press(Key.down)
                 
-    if cv2.waitKey(1) == 27:
-        cv2.destroyAllWindows()
-        break
+            # right key    
+            elif((hand[0]['lmList'][0][0] > hand[0]['lmList'][12][0] )and (hand[0]['lmList'][0][0] > hand[0]['lmList'][12][1])):
+                print("right")
+                myKeyboard.press(Key.right)
+                
+            # left key    
+            elif((hand[0]['lmList'][0][0] < hand[0]['lmList'][12][1]) and (hand[0]['lmList'][0][0] < hand[0]['lmList'][12][0] ) ):
+                print("left")
+                myKeyboard.press(Key.left)
+                
+            # up key    
+            else:
+                print("forward")
+                myKeyboard.release(Key.alt)
+                myKeyboard.release(Key.left)
+                myKeyboard.release(Key.right)
+                myKeyboard.release(Key.down)
+                myKeyboard.press(Key.up)
+        
+        else:
+            myKeyboard.release(Key.down)
+            myKeyboard.release(Key.up)
+            myKeyboard.release(Key.alt)
+            myKeyboard.release(Key.right)
+            myKeyboard.release(Key.left)   
+        
+        cv2.imshow("a" , cv2.flip(img , 1))  
+                        
+        if cv2.waitKey(1) == 27:
+            cv2.destroyAllWindows()
+            break
+    
+camera.release()
